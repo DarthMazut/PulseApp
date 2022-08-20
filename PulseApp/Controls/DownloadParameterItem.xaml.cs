d7607cf1fe2ea44f919f60af58aac917d5586b73
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2022-08-20 10:53:05 +0200

    Add QuickDownloadPage

---
+++ PulseApp/Controls/DownloadParameterItem.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace PulseApp.Controls
+{
+    public sealed partial class DownloadParameterItem : UserControl
+    {
+        #region DEPENDENCY PROPERTY
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(string), typeof(DownloadParameterItem), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register(nameof(Header), typeof(string), typeof(DownloadParameterItem), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(DownloadParameterItem), new PropertyMetadata(null));
+
+        public string Icon
+        {
+            get { return (string)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        #endregion
+
+        public DownloadParameterItem()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
--- ViewModels/QuickDownloadPageViewModel.cs
+++ ViewModels/QuickDownloadPageViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using MochaCore.Navigation;
+using MochaCore.Settings;
+using Model;
+using Model.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +12,18 @@ using System.Threading.Tasks;
 
 namespace ViewModels
 {
-    public class QuickDownloadPageViewModel : ObservableObject, INavigatable, IOnNavigatedToAsync
+    public partial class QuickDownloadPageViewModel : ObservableObject, INavigatable, IOnNavigatedToAsync
     {
+        private readonly string INITIALIZING_STATE_NAME = "Initializing";
+        private readonly string DOWNLOADING_STATE_NAME = "Downloading";
+        private readonly string MERGING_STATE_NAME = "Merging";
+        private readonly string CLEANING_STATE_NAME = "Cleaning";
+        private readonly string FINISHED_STATE_NAME = "Finished";
+        private readonly string ERROR_STATE_NAME = "Error";
+
+        private QuickDownloadNavigationData? _navigationData;
+        private CancellationTokenSource _cts = new();
+
         public QuickDownloadPageViewModel()
         {
             Navigator = new Navigator(this, NavigationServices.MainNavigationService);
@@ -17,14 +31,66 @@ namespace ViewModels
 
         public Navigator Navigator { get; }
 
-        public Task OnNavigatedToAsync(NavigationData navigationData)
+        [ObservableProperty]
+        private string? _state;
+
+        [ObservableProperty]
+        private string? _title;
+
+        [ObservableProperty]
+        private byte[]? _rawImage;
+
+        public async Task OnNavigatedToAsync(NavigationData navigationData)
         {
-            if (navigationData.Data is QuickDownloadNavigationData data)
+            if (navigationData.Data is QuickDownloadNavigationData data && data.QuickDownloadSummary is not null)
             {
+                _navigationData = data;
+                Title = data.QuickDownloadSummary.TargetFileName;
+                RawImage = await data.DownloadRawImageAsJpeg();
 
+                ISettingsSectionProvider<ApplicationSettings> sectionProvider = SettingsManager.Retrieve<ApplicationSettings>(ApplicationSettings.ID);
+                ApplicationSettings settings = await sectionProvider.LoadAsync();
+
+                YtdlpAdapter adapter = new YtdlpAdapter(settings.YtdlpPath, settings.FfmpegPath);
+                
+                DownloadJob downloadJob = new()
+                {
+                    Url = data.QuickDownloadSummary.Url,
+                    Selection = data.FormatSelection,
+                    OutputDirectory = data.QuickDownloadSummary.OutputDirectory,
+                    DownloadedFileName = data.QuickDownloadSummary.TargetFileName,
+                    CancellationToken = _cts.Token,
+                    ProgressCallback = new Progress<DownloadProgress>(DownloadProgressReceived)
+                };
+
+                if (data.MediumSelection == MediumSelection.Video)
+                {
+                    //await adapter.DownloadVideoAsync(downloadJob);
+                }
+
+                if (data.MediumSelection == MediumSelection.Music)
+                {
+                    //await adapter.DownloadMusicAsync(downloadJob);
+                }
             }
+        }
 
-            return Task.CompletedTask;
+        private void DownloadProgressReceived(DownloadProgress progress)
+        {
+            // TODO
+        }
+
+        [RelayCommand]
+        private Task Abort()
+        {
+            _cts.Cancel();
+            return Navigator.NavigateAsync(AppPages.QuickDownloadSummaryPage.Module, _navigationData);
+        }
+
+        [RelayCommand]
+        private void DebugChangeState(string parameter)
+        {
+            State = parameter;
         }
     }
 }
