4ba27a83f1812bfcd50040fac949617401696c19
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2022-08-30 17:23:14 +0200

    Add AppVersionProvider

---
+++ PulseApp/Utils/AppVersionProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Markup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+
+namespace PulseApp.Utils
+{
+    public class AppVersionProvider : MarkupExtension
+    {
+        protected override object ProvideValue()
+        {
+            PackageVersion version = Package.Current.Id.Version;
+            return $"{version.Major}.{version.Minor}.{version.Build}";
+        }
+    }
+}
